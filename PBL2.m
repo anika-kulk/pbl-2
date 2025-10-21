963b67ad2919578568ec9e3714eb17c6703bb9e5
Anika Kulkarni <112150258+anika-kulk@users.noreply.github.com>
2025-10-21 15:42:24 -0500

    Enhance kidney model with new segments and solutes
    Updated nephron model to include additional segments and solutes (S1, S2, S3 of PCT, added Ca2+). Adjusted reabsorption and secretion fractions for improved accuracy. Encapsulated everything in a function so we can pass initial concentrations and snGFR as input (so we can tweak for test cases).
    
    IMPORTANT: NEED TO CODE VECTORS FOR TEST CASES (determine the changed initial concentrations + GFR). Add to bottom once figured out.

--- PBL2.m
+++ PBL2.m
@@ -4,7 +4,7 @@
 % Ryan Nguyen, Krishna Ravi, Tatum Thompson, Felix Wang
 
 % Code Contributors:
-% Anika Kulkarni    Last Commit: 10/16/2025
+% Anika Kulkarni    Last Commit: 10/21/2025
 % Krishna Ravi      Last Commit: 10/20/2025
 % Felix Wang        Last Commit: 10/5/2025
 
@@ -20,17 +20,18 @@
 % END GOAL: line plot of flow rates of each component per nephronal unit
 % Nephronal unit = pseudomeasure of time
 
+function[molar_flow_rates, concs] = kidney_model(C0, snGFR)
 
 % Indices
 % -----------------------------
 % Chemical Constituents
-chemicals = ["Na+", "Cl-", "Urea", "Glucose", "K+", "HCO3-", "Mg2+", "PO4 3-", "Creatinine"];
-molec_weights = [22.989, 35.453, 60.056, 180.156, 39.098, 61.020, 24.305, 94.971, 113.12]; % For gram conversion
+chemicals = ["Na+", "Cl-", "Urea", "Glucose", "K+", "HCO3-", "Mg2+", "PO4 3-", "Creatinine", "Ca2+"];
+molec_weights = [22.989, 35.453, 60.056, 180.156, 39.098, 61.020, 24.305, 94.971, 113.12, 40.08]; % For gram conversion; in g/mol
 
 % Nephronal Units
-units = ["RC", "PT", "DL", "AL", "DT", "CD"];
+units = ["RC", "PT, S1", "PT, S2", "PT, S3", "DL", "AL", "DT", "CD"];
 % RC = Renal Corpuscle
-% PT = Proximal Tubule
+% PT = Proximal Tubule, split into S1, S2, and S3
 % DL = Descending Limb
 % AL = Ascending Limb
 % DT = Distal Tubule
@@ -43,29 +44,47 @@ nSol = length(chemicals);
 % Data Matrices
 % -----------------------------
 %{
-      Na+     Cl-    Urea    Gluc     K+     HCO3-   Mg2+    PO43-   Creat  
-RC | (1,1) | (1,2) | (1,3) | (1,4) | (1,5) | (1,6) | (1,7) | (1,8) | (1,9) 
-PT | (2,1) | (2,2) | (2,3) | (2,4) | (2,5) | (2,6) | (2,7) | (2,8) | (2,9)  
-DL | (3,1) | (3,2) | (3,3) | (3,4) | (3,5) | (3,6) | (3,7) | (3,8) | (3,9)    
-AL | (4,1) | (4,2) | (4,3) | (4,4) | (4,5) | (4,6) | (4,7) | (4,8) | (4,9) 
-DT | (5,1) | (5,2) | (5,3) | (5,4) | (5,5) | (5,6) | (5,7) | (5,8) | (5,9) 
-CD | (6,1) | (6,2) | (6,3) | (6,4) | (6,5) | (6,6) | (6,7) | (6,8) | (6,9) 
+      Na+     Cl-    Urea    Gluc     K+     HCO3-   Mg2+    PO43-   Creat    Ca2+
+RC | (1,1) | (1,2) | (1,3) | (1,4) | (1,5) | (1,6) | (1,7) | (1,8) | (1,9) | (1,10)
+S1 | (2,1) | (2,2) | (2,3) | (2,4) | (2,5) | (2,6) | (2,7) | (2,8) | (2,9) | (2,10)
+S2 | (3,1) | (3,2) | (3,3) | (3,4) | (3,5) | (3,6) | (3,7) | (3,8) | (3,9) | (3,10)    
+S3 | (4,1) | (4,2) | (4,3) | (4,4) | (4,5) | (4,6) | (4,7) | (4,8) | (4,9) | (4,10)  
+DL | (5,1) | (5,2) | (5,3) | (5,4) | (5,5) | (5,6) | (5,7) | (5,8) | (5,9) | (5,10)  
+AL | (6,1) | (6,2) | (6,3) | (6,4) | (6,5) | (6,6) | (6,7) | (6,8) | (6,9) | (6,10)  
+DT | (7,1) | (7,2) | (7,3) | (7,4) | (7,5) | (7,6) | (7,7) | (7,8) | (7,9) | (7,10)  
+CD | (8,1) | (8,2) | (8,3) | (8,4) | (8,5) | (8,6) | (8,7) | (8,8) | (8,9) | (8,10)  
 %}
-
-% Inlet filtrate concentrations at renal corpuscle
-C0 = [140, 102, 5.71, 4.69, 4.35, 24.00, 0.83, 0.40, 0.09]; % Na+, Cl-, Urea, Glucose, K+, HCO3-, Mg2+, PO43-, Creatinine 
-% In mmol/L
 
 % Reabsorption fractions per constituent (of incoming stream) per unit
 % This is what's returned to interstitial fluid
-reabs_frac = [0, 0, 0, 0, 0, 0, 0, 0, 0; % RC
-              0.65, 0.65, 0.50, 0.99, 0.65, 0.85, 0.25, 0.80, 0 ; % PT
-              0, 0, 0, 0, 0, 0.15, 0, 0, 0; % DL (salt-impermeable)
-              0.25, 0.25, 0, 0, 0.25, 0, 0.7, 0, 0; % AL (water-impermeable, salt-permeable)
-              0.05, 0.05, 0, 0, 0, 0, 0.05, 0.05, 0; % DT
-              0, 0, 0.03, 0.03, 0.40, 0, 0, 0, 0; % CD
+% Solutes    Na+,     Cl-,     Urea,    Glucose, K+,      HCO3-,   Mg2+,    PO43-,   Creat,   Ca2+ 
+reabs_frac = [0,       0,       0,       0,       0,       0,       0,       0,       0,       0;    % RC
+              0.33,    0.33,    0,       0.90,    0,       0.80,    0,       0.35,    0,       0.35; % PT, S1
+              0.328,   0.328,   0,       0.09,    0,       0.10,    0,       0.25,    0,       0.25; % PT, S2
+              0.222,   0.222,   0.50,    0,       0.60,    0,       0.20,    0.10,    0,       0.10; % PT, S3
+              0,       0,       0,       0,       0,       0.15,    0,       0,       0,       0;    % DL (salt-impermeable)
+              0.25,    0.25,    0,       0,       0.25,    0,       0.7,     0,       0,       0.20; % AL (water-impermeable, salt-permeable)
+              0.05,    0.05,    0,       0,       0,       0.05,    0.05,    0.05,    0,       0.10; % DT
+              0,       0,       0,       0,       0,       0,       0,       0,       0,       0.05; % CD
              ];
+% Note that Na/Cl reabsorption can increase to 0.02/0.03 when producing dilute urine (over-hydration); also when high salt intake (need to dilute urine)
+% Note that urea is reabsorbed when producing concentrated urine to conserve water, as urea establishes an osmotic gradient (de-hydration)
+      % However, because this is tightly regulated by ADH and other hormones, this is outside the scope of this model
 reabs_frac = max(0, min(reabs_frac, 0.999)); % Safety clamp
+
+% Secretion fractions per constituent (of incoming stream) per unit
+% This is what's secreted by bloodstream into tubule post-RC (initial filtration)
+% Solutes    Na+,     Cl-,     Urea,    Glucose, K+,      HCO3-,   Mg2+,    PO43-,   Creat,   Ca2+ 
+sec_frac =   [0,       0,       0,       0,       0,       0,       0,       0,       0,       0; % RC
+              0,       0,       0,       0,       0,       0,       0,       0,       0,       0; % PT, S1
+              0,       0,       0,       0,       0,       0,       0,       0,       0.30,    0; % PT, S2
+              0,       0,       0,       0,       0,       0,       0,       0,       0,       0; % PT, S3
+              0,       0,       0.15,    0,       0,       0,       0,       0,       0,       0; % DL (salt-impermeable)
+              0,       0,       0,       0,       0,       0,       0,       0,       0,       0; % AL (water-impermeable, salt-permeable)
+              0,       0,       0,       0,       0.10,    0,       0,       0,       0,       0; % DT
+              0,       0,       0,       0,       0.10,    0,       0,       0,       0,       0; % CD
+             ];
+sec_frac = max(0, min(sec_frac, 0.999)); % Safety clamp
 
 % Molar flow rates per constituent per unit
 % To be solved for; preallocated for now 
@@ -75,10 +94,6 @@ molar_flow_rates = zeros(nSeg,nSol);
 % To be solved for; preallocated for now 
 concs = zeros(nSeg,nSol); % mmol/L
 
-% Input concentrations
-snGFR = 79 / 1000; % nL/min * 1e-3 = mL/min (filtrate into RC/Bowman's capsule for a healthy kidney)
-% A healthy kidney has a single-nephron GFR of approximately 79 +/- 42 nanoliters per minute (nl/min).
-
 % Volumetric flow rates per constituent per unit
 vol_flow_rates = snGFR * ones(nSeg,1); % mL/min
 % Set up so molar flow rate = vol flow rate * conc, so mL/min * mM * 1 L/1000 mL = mmol/min
@@ -86,9 +101,19 @@ vol_flow_rates = snGFR * ones(nSeg,1); % mL/min
 % Calculations
 % -----------------------------
 % Initialize input stream to first unit (RC)
-concs(1,:) = C0; % mmol/L, aka mM
-molar_flow_rates(1,:) = snGFR .* concs(1,:) * 1e-3; % mmol/min
+concs(1,:) = C0; % mmol/L, aka mM; TAKEN AS INPUT
+molar_flow_rates(1,:) = snGFR .* concs(1,:) * 1e-3; % mmol/min; snGFR TAKEN AS INPUT
 
+% Conditional
+% -----------------------------
+% Adjust reabsorption fractions for salt in the case of high salt intake - whether a one-off event or hypernatremia/hyperchloremia
+if concs(1, 1) > 140.0000 & concs(1, 2) > 106
+      reabs_frac(8,1) = 0.02
+      reabs_frac(8,2) = 0.03
+end
+
+% Math Propagation
+% -----------------------------
 % Propagate flow rates by unit/segment
 for i = 2:nSeg
 
@@ -100,7 +125,7 @@ for i = 2:nSeg
         errordlg("Negative Flow Rate at Row " + i + "!!!")
     end
 
-    % Fill row i of 6x9 matrix
+    % Fill row i of 8x10 matrix
     molar_flow_rates(i,:) = N_out; % Given that P_out is a vector containing flow rates of all chemicals at that unit              
     concs(i,:) = molar_flow_rates(i,:) ./ vol_flow_rates(i); % mmol/min / mL/min = mmol/L
 end
@@ -119,11 +144,11 @@ end
 % Graphs
 % -----------------------------
 % Display to check matrix is reasonable
-disp('6x9 Molar Flow Rate Matrix (rows = RC, PT, DL, AL, DT, CD; columns = Na+, Cl-, Urea, Glucose, K+, HCO3-, Mg2+, phosphate, creatinine):');
+disp('8x10 Molar Flow Rate Matrix (rows = RC, S1, S2, S3, DL, AL, DT, CD; columns = Na+, Cl-, Urea, Glucose, K+, HCO3-, Mg2+, phosphate, creatinine, Ca2+):');
 disp(molar_flow_rates);
 
-colors = ["#d53e4f", "#f46d43", "#fdae61", "#fee08b", ...
-          "#ffffbf", "#e6f598", "#abdda4", "#66c2a5", "#3288bd"];
+colors = ["#001219", "#005f73", "#0a9396", "#94d2bd", "e9d8a6", ...
+          "#ee9b00", "#ca6702", "#bb3e03", "#ae2012", "#9b2226"];
 
 % Figure 1: Concentrations
 figure('Units','normalized','Position',[0.05 0.05 0.9 0.8])
@@ -159,6 +184,23 @@ for k = 1:nSol
 end
 
 sgtitle('Molar Flow Rates Along Nephron Segments', 'FontSize', 14, 'FontWeight','bold')
+
+end
+
+% Inputs
+% -----------------------------
+
+% DEFAULT TEST CASE: healthy kidney
+% Inlet filtrate concentrations at renal corpuscle, as INPUT into main function
+C0 = [140.0000, 102.0000, 5.714, 4.6905, 4.3500, 24.0000, 0.8225, 0.3950, 0.0920, 0.5700]; % Na+, Cl-, Urea, Glucose, K+, HCO3-, Mg2+, PO43-, Creatinine, Ca2+ 
+% In mmol/L
+
+% Input concentrations
+snGFR = 79 / 1000; % nL/min * 1e-3 = mL/min (filtrate into RC/Bowman's capsule for a healthy kidney)
+% A healthy kidney has a single-nephron GFR of approximately 79 +/- 42 nanoliters per minute (nL/min).
+
+kidney_model(C0, snGFR)
+
 
 
 
